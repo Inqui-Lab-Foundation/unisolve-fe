07cfd667734f4992798f2f4adf3fcee363e9113e
inquijhani <jhani@inqui-lab.org>
2022-02-15 13:58:05 +0530

    final setup

---
+++ src/Pages/UserAddteam.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserAddteam = () => {
+  return <div>UserAddteam</div>;
+};
+
+export default UserAddteam;
---
+++ src/Pages/UserPersonalDetails.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const UserPersonalDetails = ({ nextStep, prevStep, handleChange, values }) => {
+  const Continue = (e) => {
+    e.preventDefault();
+    nextStep();
+  };
+  const Previous = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
+
+  return (
+    <form>
+      <label>
+        Month
+        <input
+          type="text"
+          placeholder="Month"
+          value={values.month}
+          onChange={handleChange("month")}
+        />
+      </label>
+
+      <button onClick={Continue}>Next</button>
+      <button onClick={Previous}>Previous</button>
+    </form>
+  );
+};
+
+export default UserPersonalDetails;
