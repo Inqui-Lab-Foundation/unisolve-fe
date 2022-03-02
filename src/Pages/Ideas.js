01e99783809683edaf0cd2947c2ffe4938b780ae
shravani <shravanik@thinkebiz.net>
2022-03-02 18:40:26 +0530

    List Component Desing Changes

---
+++ src/Pages/Ideas.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { InputWithRadioComp } from "../stories/InputWithRadio/InputWithRadio";
+
+const Ideas = () => {
+  console.log("==========hiiii");
+  return (
+    <div>
+      <h1>Welcome Ideas</h1>
+    </div>
+  );
+};
+
+export default Ideas;
--- src/components/SideMenuBarComp/index.js
+++ src/components/SideMenuBarComp/index.js
@@ -2,29 +2,27 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "../../Pages/Dashboard";
 import Badges from "../../Pages/Badges";
+import Ideas from "../../Pages/Ideas";
 import Menu from "./Menu";
-import {Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "./style.scss";
 
 export default function Layout() {
   return (
     <div className="sideBar">
-        <Row className="m-0">
+      <Row className="m-0">
         <Col xs={6} md={2} className="app__sidebar">
-        <Menu />
-
-      </Col>
-      <Col xs={12} md={10} className="app__content">
-        <Switch>
-          <Route path="/about" component={Dashboard} />
-          <Route path="/badges" component={Badges} />
-          <Route path="/" component={Dashboard} />
-        </Switch>
-      </Col>
-        </Row>
-      
-
-     
+          <Menu />
+        </Col>
+        <Col xs={12} md={10} className="app__content">
+          <Switch>
+            <Route path="/about" component={Dashboard} />
+            <Route path="/badges" component={Badges} />
+            <Route path="/ideas" component={Ideas} />
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </Col>
+      </Row>
     </div>
   );
 }
