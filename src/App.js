b5dfb7b851a8283458973021a4437ffc523f6d37
shravani <shravanik@thinkebiz.net>
2022-03-04 15:32:49 +0530

    RouteAdd

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   withRouter,
   useHistory,
 } from "react-router-dom";
+import { ProtectedRoute } from "./helpers/authHelper";
 import { StudentHeaderComp } from "./stories/StudentHeaderComp/StudentHeader.stories";
 import SideMenuBarComp from "./components/SideMenuBarComp";
 import { ImageCardComp } from "./stories/ImageCard/ImageCard";
@@ -16,8 +17,6 @@ import Ideas from "./Pages/Ideas";
 import BadgesComp from "./Pages/Badges";
 import MainPage from "./Pages/MainPages";
 import "./i18n";
-
-// import SignUp from "./Pages/SignUp";
 import SignUpNew from "./Pages/SignUpNew";
 import LoginNew from "./Pages/LoginNew";
 import CreateNewPassword from "./Pages/CreateNewPassword";
@@ -27,64 +26,64 @@ import LogoutView from "./Pages/LogoutView";
 import MySettings from "./Pages/MySettings";
 import EditPersonalDetails from "./Pages/EditPersonalDetails";
 import MyProfile from "./Pages/MyProfile";
+import { getCurrentUser } from "./helpers/Utils";
 
-// import { Route } from "react-router-dom";
-// import history from "./helpers";
 const Login = React.lazy(() =>
   import(/* webpackChunkName: "views-user" */ "./views/user/login")
 );
 
 const App = () => {
+  const currentUser = getCurrentUser("current_user");
+  if (currentUser) {
+    return <Redirect exact from="/" to="dashboard" />;
+  }
   return (
     <React.Fragment>
-      {/* <Router history={history}> */}
-      <Switch>
-        <Route exact path="/" render={() => <SignUpNew />} />
-        <Route exact path="/register" render={() => <SignUpNew />} />
-        <Route exact path="/login" render={() => <LoginNew />} />
-        <Route exact path="/logout" render={() => <LogoutView />} />
-        <Route exact path="/forgotpassword" render={() => <ForgotPassword />} />
-        <Route
-          exact
-          path="/verifypassword"
-          render={() => <PasswordEmailConfirmation />}
-        />
-        <Route exact path="/settings" render={() => <MySettings />} />
-        <Route exact path="/my-profile" render={() => <MyProfile />} />
-        <Route
-          exact
-          path="/edit-details"
-          render={() => <EditPersonalDetails />}
-        />
-        <Route render={() => <h1 className="text-center">Page Not Found</h1>} />
-      </Switch>
-      {/* <MainPage /> */}
-      {/* </Router> */}
-      {/* <SignUpNew /> */}
-      {/* <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <MainPage />
-
-          <SignUpNew /> */}
-      {/* <LoginNew /> */}
-      {/* <CreateNewPassword /> */}
-      {/* <PasswordEmailConfirmation /> */}
-      {/* <ForgotPassword /> */}
-      {/* <LogoutView /> */}
-      {/* <Switch>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          {/* <MainPage /> */}
-      {/* <SignUpNew /> */}
-      {/* <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/about" component={BadgesComp} />
-         <Route component={NoMatch} />
-      </Switch> */}
-      {/* </Suspense>
-      </Router> */}
-      {/* </Suspense> */}
-      {/* </Router> */}
+        <Switch>
+          <Redirect exact from="/" to="/login" />
+          <Route exact path="/register" render={() => <SignUpNew />} />
+          <Route exact path="/login" render={() => <LoginNew />} />
+          <Route exact path="/ideas" render={() => <Ideas />} />
+          <Route path="/badges" render={() => <BadgesComp />} />
+
+          <Route
+            exact
+            path="/forgotpassword"
+            render={() => <ForgotPassword />}
+          />
+          <Route
+            exact
+            path="/create-password"
+            render={() => <CreateNewPassword />}
+          />
+          <Route
+            exact
+            path="/confirm-password"
+            render={() => <PasswordEmailConfirmation />}
+          />
+          <Route
+            exact
+            path="/verifypassword"
+            render={() => <PasswordEmailConfirmation />}
+          />
+          <Route exact path="/logout" render={() => <LogoutView />} />
+          <Route exact path="/settings" render={() => <MySettings />} />
+          <Route exact path="/my-profile" render={() => <MyProfile />} />
+          <Route
+            exact
+            path="/edit-details"
+            render={() => <EditPersonalDetails />}
+          />
+          <Route exact path="/dashboard" render={() => <MainPage />} />
+          <Route exact path="/about" render={() => <Dashboard />} />
+          <Route exact path="/courses" render={() => <Dashboard />} />
+          <Route exact path="/teams" render={() => <Dashboard />} />
+          {/* <Route exact path="/about" render={() => <Dashboard />} /> */}
+          {/* Protected Route Example // add ProtectedRoute for all authenticated Routes //
+          <ProtectedRoute exact path="/dashboard" render={() => <MainPage />} /> */}
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 };
--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-// import { isAuthGuardActive } from "constants/defaultValues";
+import { isAuthGuardActive } from "../constants/defaultValues";
 import { getCurrentUser } from "./Utils";
 
-// const ProtectedRoute = ({
-//   component: Component,
-//   // roles = undefined,
-//   ...rest
-// }) => {
-//   const setComponent = (props) => {
-//     if (isAuthGuardActive) {
-//       const currentUser = getCurrentUser();
-//       if (currentUser) {
-//         return <Component {...props} />;
-//       }
-//       return (
-//         <Redirect
-//           to={{
-//             pathname: "/user/login",
-//             state: { from: props.location },
-//           }}
-//         />
-//       );
-//     }
-//     return <Component {...props} />;
-//   };
+const ProtectedRoute = ({
+  component: Component,
+  // roles = undefined,
+  ...rest
+}) => {
+  const setComponent = (props) => {
+    if (isAuthGuardActive) {
+      const currentUser = getCurrentUser();
+      if (currentUser) {
+        return <Component {...props} />;
+      }
+      return (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location },
+          }}
+        />
+      );
+    }
+    return <Component {...props} />;
+  };
 
-//   return <Route {...rest} render={setComponent} />;
-// };
+  return <Route {...rest} render={setComponent} />;
+};
 
-// export { ProtectedRoute };
+export { ProtectedRoute };
