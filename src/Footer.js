a6025a6a8dae65520f67ea5c2bd0d9f3689867e3
inquijhani <99166839+inquijhani@users.noreply.github.com>
2022-03-28 16:31:56 +0530

    ui changes (#48)

---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <Container className="Footer">
+      <Row className="py-4">
+        <Col md={6}>
+          <Link exact to="/">
+            &copy; Unisolve @ {new Date().getFullYear()}
+          </Link>
+        </Col>
+        <Col md={6} className="text-right my-auto">
+          <p className="p-0 my-auto">
+            <span className="my-auto">&reg;</span> All righrs reserved
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
