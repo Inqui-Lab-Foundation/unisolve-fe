18786b60aeede9f3932905186d7762f64beafb54
shravani <shravanik@thinkebiz.net>
2022-04-28 07:51:35 +0530

    New Common Components In StoryBook

---
+++ src/stories/FileComp/FileComp.stories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FileComp } from "./FileComp";
+
+//Export Slider component from FileComp.jsx
+
+export default {
+  title: "Example/FileComp",
+  component: FileComp,
+  argTypes: { label: "Select File" },
+};
+
+//👇 We create a “template” of how args FileComp to rendering
+const Template = (args) => <FileComp {...args} />;
+
+export const Select = Template.bind({});
+Select.args = {
+  label: "Select File",
+};
---
+++ src/stories/SelectComp/SelectComp.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+// import "antd/dist/antd.css";
+// import "./index.css";
+import { Select } from "antd";
+const { Option } = Select;
+
+export const SelectComp = ({ options, label, ...props }) => {
+  const children = [];
+
+  for (let i = 0; i < options.length; i++) {
+    children.push(<Option key={options[i]}>{options[i]}</Option>);
+  }
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  console.log("======options", options);
+  return (
+    <Select
+      mode="multiple"
+      allowClear
+      style={{ width: "100%" }}
+      placeholder="Please select"
+      //   defaultValue={[]}
+      onChange={handleChange}
+    >
+      {children}
+    </Select>
+  );
+};
+
+// SelectComp.propTypes = {
+//   onClick: PropTypes.func,
+// };
+
+SelectComp.defaultProps = {
+  label: "Select",
+  options: ["Garde 1", "Garde 2", "Garde 3", "Garde 4", "Garde 5", "Garde 6"],
+};
