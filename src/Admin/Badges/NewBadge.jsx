dbf9f2bbbc1a06df2a77df312b937389b035e004
inquijhani <jhani@inqui-lab.org>
2022-06-17 19:17:49 +0530

    UI changes

--- src/Admin/Badges/NewBadge.jsx
+++ src/Admin/Badges/NewBadge.jsx
@@ -64,39 +64,39 @@ const NewBadge = (props) => {
 
   return (
     <Layout>
-      <div className="EditPersonalDetails new-ticket-page">
-        <Row className="register-block bg-transparent">
-          <Col className="col-xl-10 offset-xl-1 offset-md-0">
+      <div className='EditPersonalDetails new-ticket-page'>
+        <Row className='register-block bg-transparent'>
+          <Col className='col-xl-10 offset-xl-1 offset-md-0'>
             <BreadcrumbTwo {...headingDetails} />
 
             <div>
               {/* <Col>
                 <h1 className="mb-4">Raise a new ticket</h1>
               </Col> */}
-              <div className="create-ticket">
-                <p className="m-0 question">Name</p>
-                <span className="que-text">
+              <div className='create-ticket'>
+                <p className='m-0 question'>Name</p>
+                <span className='que-text'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </span>
                 <InputBox
                   {...inputIdeaTitle}
-                  id="ideaTitle"
-                  name="ideaTitle"
+                  id='ideaTitle'
+                  name='ideaTitle'
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.ideaTitle}
                 />
-                <p className="m-0 question mt-5">Description</p>
-                <span className="que-text">
+                <p className='m-0 question mt-5'>Description</p>
+                <span className='que-text'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </span>
-                <TextArea placeholder="Enter your question description here..." />
+                <TextArea placeholder='Enter your question description here...' />
 
-                <p className="m-0 question mt-5">Points count</p>
+                <p className='m-0 question mt-5'>Points count</p>
 
                 <ProgressComp {...progressBar} />
 
-                <p className="m-0 question mt-5 mb-3">Badge icon</p>
+                <p className='m-0 question mt-5 mb-3'>Badge icon</p>
 
                 <Row>
                   <Col md={3}>
@@ -110,19 +110,19 @@ const NewBadge = (props) => {
               <Row>
                 <Col>
                   <Button
-                    label="Discard"
-                    btnClass="secondary"
-                    size="small"
+                    label='Discard'
+                    btnClass='secondary'
+                    size='small'
                     // onClick={() => props.history.push("/tickets")}
                   />
                 </Col>
-                <Col className="submit-btn">
+                <Col className='submit-btn'>
                   <Button
-                    label="Save as Draft"
-                    btnClass="primary-outline"
-                    size="small"
+                    label='Save as Draft'
+                    btnClass='primary-outline'
+                    size='small'
                   />
-                  <Button label="Submit" btnClass="default" size="small" />
+                  <Button label='Submit' btnClass='default' size='small' />
                 </Col>
               </Row>
             </div>
---
+++ src/context/quiz.context.js
@@ -0,0 +1,68 @@
+import { createContext, useReducer } from "react";
+import { shuffleAnswers } from "../Admin/Quiz/helpers";
+import questions from "../Admin/Quiz/quiz-data";
+
+const initialState = {
+  questions,
+  currentQuestionIndex: 0,
+  currentAnswer: "",
+  answers: shuffleAnswers(questions[0]),
+  showResults: false,
+  correctAnswersCount: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SELECT_ANSWER": {
+      const correctAnswersCount =
+        action.payload ===
+        state.questions[state.currentQuestionIndex].correctAnswer
+          ? state.correctAnswersCount + 1
+          : state.correctAnswersCount;
+      return {
+        ...state,
+        currentAnswer: action.payload,
+        correctAnswersCount,
+      };
+    }
+    case "NEXT_QUESTION": {
+      const showResults =
+        state.currentQuestionIndex === state.questions.length - 1;
+      const currentQuestionIndex = showResults
+        ? state.currentQuestionIndex
+        : state.currentQuestionIndex + 1;
+      const answers = showResults
+        ? []
+        : shuffleAnswers(state.questions[currentQuestionIndex]);
+      return {
+        ...state,
+        currentAnswer: "",
+        showResults,
+        currentQuestionIndex,
+        answers,
+      };
+    }
+    case "PREV_QUESTION": {
+      const currentQuestionIndex = state.currentQuestionIndex - 1;
+
+      return {
+        ...state,
+        currentAnswer: "",
+
+        currentQuestionIndex,
+      };
+    }
+    case "RESTART": {
+      return initialState;
+    }
+    default:
+      return state;
+  }
+};
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children }) => {
+  const value = useReducer(reducer, initialState);
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+};
