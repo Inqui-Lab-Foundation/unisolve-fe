8459dd107b3b16b5411e25a50ef9eccd0f154cfd
shravani <shravanik@thinkebiz.net>
2022-06-21 14:08:55 +0530

    Alert & Pull Notications API Done

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -7,7 +7,7 @@ export const UserRole = {
 export const URL = {
   // Post//
   login: "http://15.207.254.154:3002/api/v1/student/login",
-  adminLogin: "http://15.207.254.154:3002/api/v1/admin/login",
+  adminLogin: "http://15.207.254.154:3002/api/v1/auth/login",
   addMentor: "http://15.207.254.154:3002/api/v1/mentor/create",
   addAdminCourses: "http://15.207.254.154:3002/api/v1/course/create",
 
@@ -20,11 +20,12 @@ export const URL = {
 
   //Get//
   logOut: "http://15.207.254.154:3002/api/v1/student/logout",
-  adminLogOut: "http://15.207.254.154:3002/api/v1/admin/logout",
+  adminLogOut: "http://15.207.254.154:3002/api/v1/auth/logout",
   getMentors: "http://15.207.254.154:3002/api/v1/mentor/list",
   getModules: "http://15.207.254.154:3002/api/v1/modules/list",
   getAdminCouses: "http://15.207.254.154:3002/api/v1/course/list",
   getAdminEvaluator: "http://15.207.254.154:3002/api/v1/evaluator/list",
+  getNotificationsList: "http://15.207.254.154:3002/api/v1//notifications/tome",
 };
 const API = "O10ZPA0jZS38wP7cO9EhI3jaDf24WmKX62nWw870";
 
