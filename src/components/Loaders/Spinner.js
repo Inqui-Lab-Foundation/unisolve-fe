32f4a5dcc783ed665f8da7cb44238fb2125cd923
inquijhani <jhani@inqui-lab.org>
2022-07-26 17:44:31 +0530

    created loader component

---
+++ src/components/Loaders/Spinner.js
@@ -0,0 +1,11 @@
+import "./Loader.scss";
+const Spinner = () => {
+  return (
+    <div className='spinner-round'>
+      <div class='double-bounce1'></div>
+      <div class='double-bounce2'></div>
+    </div>
+  );
+};
+
+export default Spinner;
