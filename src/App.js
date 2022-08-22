42569e48b793e0733d25c8a1ffeb3216d8a150c8
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2022-08-22 18:21:37 +0530

    checking with app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { IntlProvider } from "react-intl";
 import messages from "./messages";
 import Routes from "./Routes";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+// import { env } from './env'
 
 function App() {
+  console.log('REACT_PORT', process.env.REACT_PORT);
   return (
     <IntlProvider locale="en">
       <Router>
