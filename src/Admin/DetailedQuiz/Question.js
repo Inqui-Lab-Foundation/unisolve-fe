fbdec4699cfa513bd703b7f5469a4e2898a09f94
shravani <shravanik@thinkebiz.net>
2022-08-24 10:59:06 +0530

    MCQ Done

--- src/Admin/DetailedQuiz/Question.js
+++ src/Admin/DetailedQuiz/Question.js
@@ -1,25 +1,25 @@
 // import { useContext } from 'react';
 // import { DetailedQuizContext } from '../../context/detailquiz.context';
+import { React, useEffect, useState } from 'react';
 import { Fragment } from 'react';
 import { FormGroup, Input, Label } from 'reactstrap';
 const config = process.env.REACT_APP_API_IMAGE_BASE_URL;
 const Question = (props) => {
-    // const topicId = props.quizId;
+    const [isCheck, setIsCheck] = useState([]);
+
     const quiz = props.adminQuizDetails ? props.adminQuizDetails : [];
-    // const [quizState, dispatch] = useContext(DetailedQuizContext);
-    // const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
-    // const onSelectAnswer = (answer) => {
-    //   alert(answer);
-    //   const body = JSON.stringify({
-    //     quiz_question_id: quiz[0].quiz_question_id,
-    //     selected_option: answer,
-    //   });
-    //   props.getAdminQuizResponce(body);
-    // };
-    console.log(
-        'quiz[0] && quiz[0].questio==',
-        config + quiz[0].question_image
-    );
+    const handleClick = (e) => {
+        const { name, checked } = e.target;
+        setIsCheck([...isCheck, name]);
+        if (!checked) {
+            setIsCheck(isCheck.filter((item) => item !== name));
+        }
+    };
+    useEffect(() => {
+        const selectedAns = isCheck.toString();
+        const allAns = selectedAns.replace(/,/g, '{{}}');
+        props.onSelectAnswer(allAns);
+    }, [isCheck]);
     return (
         <Fragment>
             {quiz[0].question_image != null ? (
@@ -37,34 +37,44 @@ const Question = (props) => {
                     quiz[0].options.map((answer) => {
                         const file = answer.split('.', 2);
                         return (
-                            <div
-                                className={'answer '}
-                                onClick={() => props.onSelectAnswer(answer)}
-                                key={answer.id}
-                            >
-                                <FormGroup check className="answer-text">
-                                    <Input
-                                        className="my-auto"
-                                        name="radio1"
-                                        type="radio"
-                                    />{' '}
-                                    {file[1] === 'png' ? (
-                                        <figure className="text-center my-auto mx-3">
-                                            <img
-                                                // src={
-                                                //   "https://www.freepnglogos.com/uploads/globe-png/globe-memberships-frontier-trainings-7.png"
-                                                // }
-                                                src={config + answer}
-                                                alt={answer}
-                                                className="img-fluid"
-                                                style={{ width: '50px' }}
-                                            />
-                                        </figure>
-                                    ) : (
-                                        <Label className="px-3">{answer}</Label>
-                                    )}
-                                    {/* <Label className="px-3">{answer}</Label> */}
-                                </FormGroup>
+                            <div className={'answer '}>
+                                {quiz[0] && quiz[0].type == 'MCQ' ? (
+                                    <label class="my-auto mx-3">
+                                        <input
+                                            name={answer}
+                                            type="checkbox"
+                                            class="mx-2"
+                                            onChange={handleClick}
+                                            isChecked={isCheck.includes(answer)}
+                                        />
+                                        {answer}
+                                    </label>
+                                ) : (
+                                    <FormGroup check className="answer-text">
+                                        <Input
+                                            onChange={() =>
+                                                props.onSelectAnswer(answer)
+                                            }
+                                            className="my-auto"
+                                            name="radio1"
+                                            type="radio"
+                                        />{' '}
+                                        {file[1] === 'png' ? (
+                                            <figure className="text-center my-auto mx-3">
+                                                <img
+                                                    src={config + answer}
+                                                    alt={answer}
+                                                    className="img-fluid"
+                                                    style={{ width: '50px' }}
+                                                />
+                                            </figure>
+                                        ) : (
+                                            <Label className="px-3">
+                                                {answer}
+                                            </Label>
+                                        )}
+                                    </FormGroup>
+                                )}
                             </div>
                         );
                     })}
