f7da983eacb70ed6d7ed6aa6b95d089c7a4d2d65
vishnu-inqui <vishnu@inqui-lab.org>
2022-09-21 11:24:05 +0530

    added a button on course completion page

--- src/Student/Pages/Courses/CourseSuccessMessage.jsx
+++ src/Student/Pages/Courses/CourseSuccessMessage.jsx
@@ -1,7 +1,11 @@
 import Confetti from 'react-confetti';
+import { useHistory } from 'react-router-dom';
 import ResultStar from '../../../assets/media/quiz-result-star.png';
+import { Button } from '../../../stories/Button';
+
 
 const CourseSuccessMessage = () => {
+    const history = useHistory();
     return (
         <div className="container new-result">
             <Confetti className="w-100" />
@@ -13,6 +17,16 @@ const CourseSuccessMessage = () => {
                     </div>
                     <div className="congratulations text-center">
                         Course Completed Successfully!
+                    </div>
+                    <div className='text-center'>
+                        <Button
+                            label="Go to Challenges"
+                            btnClass="primary mt-4"
+                            size="small"
+                            onClick={() =>
+                                history.push("/challenges")
+                            }
+                        />
                     </div>
                 </div>
             </div>
